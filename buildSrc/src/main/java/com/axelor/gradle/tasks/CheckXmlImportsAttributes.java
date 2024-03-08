2697b42d5691493ae9945ce8db727590a7c3ca23
ale-axelor <25925065+ale-axelor@users.noreply.github.com>
2024-03-08 16:14:58 +0100

    fixup! RM#74613 Add a new CLI task to check xml attributes validity (#95)
    (cherry picked from commit 8982d9172c0b4e89fba36e677ed8f210af907d60)

--- buildSrc/src/main/java/com/axelor/gradle/tasks/CheckXmlImportsAttributes.java
+++ buildSrc/src/main/java/com/axelor/gradle/tasks/CheckXmlImportsAttributes.java
@@ -1,9 +1,5 @@
 package com.axelor.gradle.tasks;
 
-import com.axelor.gradle.tasks.xmlcheck.XmlLineException;
-import java.io.File;
-import org.gradle.api.tasks.TaskAction;
-
 public class CheckXmlImportsAttributes extends AbstractCheckXmlAttributes {
 
   protected static final String SCHEMA_PATH = "http://axelor.com/xml/ns/data-import";
